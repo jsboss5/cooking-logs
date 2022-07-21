0abf282d4b11aee731061c18a34dfeb508f4698c
Joshua Boss <joshua.boss@duke.edu>
2022-07-21 16:16:10 -0400

    add js pipeline

---
+++ chefin/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  entry: './front-end/index.js',  // path to our input file
+  output: {
+    filename: 'index-bundle.js',  // output bundle file name
+    path: path.resolve(__dirname, './static'),  // path to our Django static directory
+  },
+};
