d4520325c04216009e0a4b22418b8d198931d2c8
Joshua Boss <joshua.boss@duke.edu>
2022-07-21 17:03:11 -0400

    got react working with babel and webpack. should be able to serve react pages now

--- chefin/front-end/index.js
+++ chefin/front-end/index.js
@@ -1,8 +1,7 @@
+import React from 'react';
+import ReactDOM from "react-dom";
 
-
-function component() {
-    const element = document.createElement('div');
-    element.innerHTML = 'Hello webpack';
-    return element;
-  }
-  document.body.appendChild(component());
+ReactDOM.render(
+  <h1>Hello, react!</h1>,
+  document.getElementById('root')
+);
