359d38c2a79d38291ee55908f4fe96714b6ba57c
Joshua Boss <joshua.boss@duke.edu>
2022-07-21 16:53:21 -0400

    webpack helloworlld

--- chefin/chefin/urls.py
+++ chefin/chefin/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.views.generic.base import TemplateView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello-webpack/', TemplateView.as_view(template_name='base.html'))
+    path('hello-webpack/', TemplateView.as_view(template_name='recipes/base.html'))
 ]
 
--- chefin/static/index-bundle.js
+++ chefin/static/index-bundle.js
@@ -21,12 +21,12 @@ eval("\n\nfunction component() {\n    const element = document.createElement('di
 
 /******/ 	});
 /************************************************************************/
-/******/
+/******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
 /******/ 	__webpack_modules__["./front-end/index.js"]();
-/******/
+/******/ 	
 /******/ })()
 ;
